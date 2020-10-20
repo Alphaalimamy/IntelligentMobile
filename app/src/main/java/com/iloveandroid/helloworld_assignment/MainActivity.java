cf0fe500d849aa27e74af2aedb17634fa8b4e4af
ALPHA ALIMAMY <alphaalimamykamara@gmail.com>
2020-10-20 18:41:45 +0800

    Hello world Assignment

---
+++ app/src/main/java/com/iloveandroid/helloworld_assignment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.iloveandroid.helloworld_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        Log.i("MainActivity", "MainActivity layout is complete");
+
+    }
+}
