428e82d5a1c8061bd75683f54a6b757434ab6ff9
ALPHA ALIMAMY <alphaalimamykamara@gmail.com>
2020-10-28 21:59:26 +0800

    TabLayout

---
+++ app/src/main/java/com/iloveandroid/colabtablayout/PagerAdapter.java
@@ -0,0 +1,37 @@
+package com.iloveandroid.colabtablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm,  int numOfTabs) {
+        super( fm );
+        this.numOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new TopStories();
+            case 1:
+                return new TechNews();
+            case 2:
+                return new Cooking();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
